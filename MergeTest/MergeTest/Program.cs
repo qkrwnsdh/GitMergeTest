8a045d53b6a0e5308dd2e6b167af91d1250a9b0f
DESKTOP-EIVOOOA\ASUS <qkrwnsdh00@gmail.com>
2023-07-03 16:27:25 +0900

    update: Ruler Class Add _SetInch function

--- MergeTest/MergeTest/Program.cs
+++ MergeTest/MergeTest/Program.cs
@@ -8,32 +8,35 @@ namespace MergeTest
 {
     internal class Program
     {
-        // {        2023.   07. 03. Modified class position / people3
-        public class Rular
-        {
-            private const float ONE_INCH = 2.54F;
-
-            public int Centimeter { get; set; } = 0;
-
-            public float Inch
-            {
-                get { return Centimeter * ONE_INCH; }
-                private set { Centimeter = (int)(value / ONE_INCH); }
-            }
-
-            public Rular(int cmValue) { Centimeter = cmValue; }
-
-            public void Run()
-            {
-                Console.WriteLine($"{this.Centimeter}cm 는 {this.Inch}inch 입니다.");
-            }
-        }
-        // }        2023.   07. 03. Modified class position / people3
         static void Main(string[] args)
         {
             Rular rular = new Rular(10);
             rular.Run();
         }
+    }
 
+    public class Rular
+    {
+        private const float ONE_INCH = 2.54F;
+
+        public int Centimeter { get; set; } = 0;
+
+        public float Inch
+        {
+            get { return Centimeter * ONE_INCH; }
+
+            // {    2023.   07. 03      Add private _SetInch function   /   people3
+            private set { this._SetInch(value); }
+        }
+
+        public Rular(int cmValue) { Centimeter = cmValue; }
+
+        public void Run()
+        {
+            Console.WriteLine($"{this.Centimeter}cm 는 {this.Inch}inch 입니다.");
+        }
+
+        private void _SetInch(float inchValue) { Centimeter = (int)(inchValue / ONE_INCH); }
+        // }    2023.   07. 03      Add private _SetInch function   /   people3
     }
 }
