ed04e44adcd814b290aae1b0fdb0e268bcbb3737
DESKTOP-EIVOOOA\ASUS <qkrwnsdh00@gmail.com>
2023-07-03 14:56:27 +0900

    Add: Add Rular class setup ok

--- MergeTest/MergeTest/Program.cs
+++ MergeTest/MergeTest/Program.cs
@@ -10,7 +10,28 @@ namespace MergeTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Rular rular = new Rular(10);
+            rular.Run();
+        }
+
+        public class Rular
+        {
+            private const float ONE_INCH = 2.54F;
+
+            public int Centimeter { get; set; } = 0;
+
+            public float Inch
+            {
+                get { return Centimeter * ONE_INCH; }
+                private set { Centimeter = (int)(value / ONE_INCH); }
+            }
+
+            public Rular(int cmValue) { Centimeter = cmValue; }
+
+            public void Run()
+            {
+                Console.WriteLine($"{this.Centimeter}cm 는 {this.Inch}inch 입니다.");
+            }
         }
     }
 }
