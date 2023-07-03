82fdba2f2aa8a4546284abd42a699636ea7a1f11
DESKTOP-EIVOOOA\ASUS <qkrwnsdh00@gmail.com>
2023-07-03 16:18:23 +0900

    update: Ruler class user input feature

--- MergeTest/MergeTest/Program.cs
+++ MergeTest/MergeTest/Program.cs
@@ -8,32 +8,40 @@ namespace MergeTest
 {
     internal class Program
     {
-        // {        2023.   07. 03. Modified class position / people2
-        public class Rular
-        {
-            private const float ONE_INCH = 2.54F;
-
-            public int Centimeter { get; set; } = 0;
-
-            public float Inch
-            {
-                get { return Centimeter * ONE_INCH; }
-                private set { Centimeter = (int)(value / ONE_INCH); }
-            }
-
-            public Rular(int cmValue) { Centimeter = cmValue; }
-
-            public void Run()
-            {
-                Console.WriteLine($"{this.Centimeter}cm 는 {this.Inch}inch 입니다.");
-            }
-        }
-        // }        2023.   07. 03. Modified class position / people2
         static void Main(string[] args)
         {
+            // {    2023. 07. 03. Add user input / people2
+            string uesrInput = string.Empty;
+            Console.WriteLine("This program convert Cm to Inch");
+            Console.WriteLine("Input Cm value : ");
+            uesrInput = Console.ReadLine();
+
+            int cmInput = 0;
+            int.TryParse(uesrInput, out cmInput);
+
             Rular rular = new Rular(10);
+            // }    2023. 07. 03. Add user input / people2
             rular.Run();
         }
+    }
 
+    public class Rular
+    {
+        private const float ONE_INCH = 2.54F;
+
+        public int Centimeter { get; set; } = 0;
+
+        public float Inch
+        {
+            get { return Centimeter * ONE_INCH; }
+            private set { Centimeter = (int)(value / ONE_INCH); }
+        }
+
+        public Rular(int cmValue) { Centimeter = cmValue; }
+
+        public void Run()
+        {
+            Console.WriteLine($"{this.Centimeter}cm 는 {this.Inch}inch 입니다.");
+        }
     }
 }
